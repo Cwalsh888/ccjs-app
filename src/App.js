1c002f4800fd30ef9c4296f0b0361644980756f7
unknown <chicagonewsdiff@gmail.com>
2023-02-16 20:09:10 -0600

    text

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Let's see how fast this updates
         </p>
         <a
           className="App-link"
