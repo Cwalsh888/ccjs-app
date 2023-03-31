7a422df6cfe871bc93bae26b9dd5d14ac1da79fb
unknown <fictionary88@gmail.com>
2023-03-31 17:34:56 -0500

    Correct data coming through historical page

--- src/Main/index.js
+++ src/Main/index.js
@@ -33,42 +33,43 @@ const Main = () => {
   useEffect(() => {
     console.log(data);
     if (data.length > 0) {
-      setNewData(data.map(ele => {
+      setNewData(data.map((ele) => {
         const container = {};
 
-        let setup = ele.jobs.find(ele => ele.name === 'set up')?.jobassignments.length;
-        let setupCar = ele.jobs.find(ele => ele.name === 'set up car (daily supply needs pickup)')?.jobassignments.length;
-        let setupVan = ele.jobs.find(ele => ele.name === 'set up van driver')?.jobassignments.length;
-        let firstShiftDriving = ele.jobs.find(ele => ele.name === 'driving')?.jobassignments.length;
-        let firstShiftOTG = ele.jobs.find(ele => ele.name === 'otg')?.jobassignments.length;
-        let drivingFirstShift = ele.jobs.find(ele => ele.name === 'driving (first shift)')?.jobassignments.length;
-        let otgFirstShift = ele.jobs.find(ele => ele.name === 'otg (first shift)')?.jobassignments.length;
-        let secondShiftDriving = ele.jobs.findLast(ele => ele.name === 'driving')?.jobassignments.length;
-        let secondShiftOTG = ele.jobs.findLast(ele => ele.name === 'otg')?.jobassignments.length;
-        let drivingSecondShift = ele.jobs.find(ele => ele.name === 'driving (second shift)')?.jobassignments.length;
-        let otgSecondShift = ele.jobs.find(ele => ele.name === 'otg (second shift)')?.jobassignments.length;
-        let breakdown = ele.jobs.find(ele => ele.name === 'breakdown')?.jobassignments.length;
-        let breakdownCar = ele.jobs.find(ele => ele.name === 'breakdown car')?.jobassignments.length;
-        let breakdownVan = ele.jobs.find(ele => ele.name === 'breakdown van driver')?.jobassignments.length;
+        let setup = ele.jobs.find(ele => ele.name === 'set up')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+        let setupCar = ele.jobs.find(ele => ele.name === 'set up car (daily supply needs pickup)')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+        let setupVan = ele.jobs.find(ele => ele.name === 'set up van driver')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+        let firstShiftDriving = ele.jobs.find(ele => ele.name === 'driving')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+        let firstShiftOTG = ele.jobs.find(ele => ele.name === 'otg')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+        let drivingFirstShift = ele.jobs.find(ele => ele.name === 'driving (first shift)')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+        let otgFirstShift = ele.jobs.find(ele => ele.name === 'otg (first shift)')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+        let secondShiftDriving = ele.jobs.findLast(ele => ele.name === 'driving')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+        let secondShiftOTG = ele.jobs.findLast(ele => ele.name === 'otg')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+        let drivingSecondShift = ele.jobs.find(ele => ele.name === 'driving (second shift)')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+        let otgSecondShift = ele.jobs.find(ele => ele.name === 'otg (second shift)')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+        let breakdown = ele.jobs.find(ele => ele.name === 'breakdown')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+        let breakdownCar = ele.jobs.find(ele => ele.name === 'breakdown car')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+        let breakdownVan = ele.jobs.find(ele => ele.name === 'breakdown van driver')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
         let fullDay = (firstShiftDriving + firstShiftOTG >= 2 || drivingFirstShift + otgFirstShift >= 2) &&
-                      (secondShiftDriving + secondShiftOTG >= 2 || drivingSecondShift + otgSecondShift >= 2)
+                      (secondShiftDriving + secondShiftOTG >= 2 || drivingSecondShift + otgSecondShift >= 2);
 
-        container.date = ele.edate;
-        container.setup = setup;
-        container.setupCar = setupCar;
-        container.setupVan = setupVan;
-        container.firstShiftDriver = firstShiftDriving;
-        container.firstShiftOTG = firstShiftOTG;
-        container.drivingFirstShift = drivingFirstShift;
-        container.otgFirstShift = otgFirstShift;
-        container.secondShiftDriver = secondShiftDriving;
-        container.secondShiftOTG = secondShiftOTG;
-        container.drivingSecondShift = drivingSecondShift;
-        container.otgSecondShift = otgSecondShift;
-        container.breakdown = breakdown;
-        container.breakdownCar = breakdownCar;
-        container.breakdownVan = breakdownVan;
-        container.fullDay = fullDay;
+
+        container.date = ele.edate ? ele.edate : null;
+        container.setup = setup ? setup : null;
+        container.setupCar = setupCar ? setupCar : null;
+        container.setupVan = setupVan ? setupVan : null;
+        container.firstShiftDriver = firstShiftDriving ? firstShiftDriving : null;
+        container.firstShiftOTG = firstShiftOTG ? firstShiftOTG : null;
+        container.drivingFirstShift = drivingFirstShift ? drivingFirstShift : null;
+        container.otgFirstShift = otgFirstShift ? otgFirstShift : null;
+        container.secondShiftDriver = secondShiftDriving ? secondShiftDriving : null;
+        container.secondShiftOTG = secondShiftOTG ? secondShiftOTG : null;
+        container.drivingSecondShift = drivingSecondShift ? drivingSecondShift : null;
+        container.otgSecondShift = otgSecondShift ? otgSecondShift : null;
+        container.breakdown = breakdown ? breakdown : null;
+        container.breakdownCar = breakdownCar ? breakdownCar : null;
+        container.breakdownVan = breakdownVan ? breakdownVan : null;
+        container.fullDay = fullDay ? fullDay : null;
 
         return container;
       }));
@@ -77,7 +78,7 @@ const Main = () => {
 
   useEffect(() => {
     if (newData) {
-      // console.log(newData);
+      console.log(newData);
       setTodaysData(newData.find(item => item.date === today));
     }
   }, [newData, today]);
--- src/Pages/Historical/index.js
+++ src/Pages/Historical/index.js
@@ -1,32 +1,32 @@
-import { useEffect, useState } from 'react';
 import { FlexBox, FlexItems } from './styled';
 
 const Historical = (props) => {
   const { newData } = props;
-  
+
   let dataList = [];
   let today = new Date();
   newData.forEach((data, index) => {
     if (today > new Date(data.date)) {
-      let month = new Date(data.date).getMonth() +  1;
-      dataList.push(<FlexItems fullDay={data.fullDay}>{month}</FlexItems>)
+      let month = data.date.substring(5,7);
+      let day = data.date.substring(8,10);
+      dataList.push(<FlexItems fullDay={data.fullDay}>{month}/{day}</FlexItems>)
     }
-  })
+  });
   return (
     <>
-    <h1>
-      This is the Historical Page!
-    </h1>
-    <FlexBox>
-     <FlexItems>Su</FlexItems>
-     <FlexItems>Mon</FlexItems>
-     <FlexItems>Tues</FlexItems>
-     <FlexItems>Wed</FlexItems>
-     <FlexItems>Thurs</FlexItems>
-     <FlexItems>Fri</FlexItems>
-     <FlexItems>Sat</FlexItems>
-      {dataList}
-    </FlexBox>
+      <h1>
+        This is the Historical Page!
+      </h1>
+      <FlexBox>
+        <FlexItems>Mon</FlexItems>
+        <FlexItems>Tues</FlexItems>
+        <FlexItems>Wed</FlexItems>
+        <FlexItems>Thurs</FlexItems>
+        <FlexItems>Fri</FlexItems>
+        <FlexItems>Sat</FlexItems>
+        <FlexItems>Su</FlexItems>
+        {dataList}
+      </FlexBox>
     </>
   );
 }
