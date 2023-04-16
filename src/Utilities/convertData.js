aee6888bc4c1bb0e048d0b5246ef67bab3ac3a18
unknown <fictionary88@gmail.com>
2023-04-16 13:15:03 -0500

    Fix Comment Edge Case based on no ven setup on Sunday

--- src/Utilities/convertData.js
+++ src/Utilities/convertData.js
@@ -23,14 +23,13 @@ export const convertData = (data) => {
         
         let shift1stComments = ele.jobs.find(ele => ele.name === 'otg (first shift)')?.comments?.concat(ele.jobs.find(ele => ele.name === 'driving (first shift)')?.comments);
         let shift2ndComments = ele.jobs.find(ele => ele.name === 'otg (second shift)')?.comments?.concat(ele.jobs.find(ele => ele.name === 'driving (second shift)')?.comments);
-        let setupComments = ele.jobs.find(ele => ele.name === 'set up car (daily supply needs pickup)')?.comments?.concat(ele.jobs.find(ele => ele.name === 'set up van driver')?.comments);
+        let setupComments = ele.jobs.find(ele => ele.name === 'set up van driver')?.comments?.concat(ele.jobs.find(ele => ele.name === 'set up car (daily supply needs pickup)')?.comments);
         let breakdownComments = ele.jobs.find(ele => ele.name === 'breakdownCar')?.comments?.concat(ele.jobs.find(ele => ele.name === 'breakdown van driver')?.comments);
 
         let halfDay = (firstShiftDriving + firstShiftOTG >= 2 || drivingFirstShift + otgFirstShift >= 2) ||
                       (secondShiftDriving + secondShiftOTG >= 2 || drivingSecondShift + otgSecondShift >= 2);
         let fullDay = (firstShiftDriving + firstShiftOTG >= 2 || drivingFirstShift + otgFirstShift >= 2) &&
                       (secondShiftDriving + secondShiftOTG >= 2 || drivingSecondShift + otgSecondShift >= 2);    
-
 
         container.date = ele.edate ? ele.edate : null;
         container.month = ele.edate ? ele.edate.substring(5,7) : null;
