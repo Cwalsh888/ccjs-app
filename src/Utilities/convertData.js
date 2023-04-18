4df94c234bd000fb58d1305c4b6320a881613076
unknown <fictionary88@gmail.com>
2023-04-18 10:32:12 -0500

    Updating all utilities with legacy data

--- src/Utilities/convertData.js
+++ src/Utilities/convertData.js
@@ -4,45 +4,59 @@ export const convertData = (data) => {
   if (data.length > 0) {
     result = data.map(ele => {
       const container = {};
-      // Specify legacy shifts versus current shifts
       
-      let setup = ele.jobs.find(ele => ele.name === 'set up')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
-      let setupCar = ele.jobs.find(ele => ele.name === 'set up car (daily supply needs pickup)')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
-      let setupVan = ele.jobs.find(ele => ele.name === 'set up van driver')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
-      let firstShiftDriving = ele.jobs.find(ele => ele.name === 'driving')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
-      let firstShiftOTG = ele.jobs.find(ele => ele.name === 'otg')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
-      let drivingFirstShift = ele.jobs.find(ele => ele.name === 'driving (first shift)')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
-      let otgFirstShift = ele.jobs.find(ele => ele.name === 'otg (first shift)')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
-      let secondShiftDriving = ele.jobs.findLast(ele => ele.name === 'driving')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
-      let secondShiftOTG = ele.jobs.findLast(ele => ele.name === 'otg')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
-      let drivingSecondShift = ele.jobs.find(ele => ele.name === 'driving (second shift)')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
-      let otgSecondShift = ele.jobs.find(ele => ele.name === 'otg (second shift)')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
-      let breakdown = ele.jobs.find(ele => ele.name === 'breakdown')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
-      let breakdownCar = ele.jobs.find(ele => ele.name === 'breakdown car')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
-      let breakdownVan = ele.jobs.find(ele => ele.name === 'breakdown van driver')?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+      const legacySetup  = ele.jobs.find(ele => ele.name === 'set up');
+      const legacyFirstShiftOTG = ele.jobs.find(ele => ele.name === 'otg');
+      const legacyFirstShiftDrivers = ele.jobs.find(ele => ele.name === 'driving');
+      const legacySecondShiftDrivers = ele.jobs.findLast(ele => ele.name === 'driving');
+      const legacySecondShiftOTG = ele.jobs.findLast(ele => ele.name === 'otg');
+      const legacyBreakdown = ele.jobs.find(ele => ele.name === 'breakdown');
+      const setUpCar = ele.jobs.find(ele => ele.name === 'set up car (daily supply needs pickup)');
+      const setupVan = ele.jobs.find(ele => ele.name === 'set up van driver');
+      const firstShiftDrivers = ele.jobs.find(ele => ele.name === 'driving (first shift)');
+      const firstShiftOTG = ele.jobs.find(ele => ele.name === 'otg (first shift)');
+      const secondShiftDrivers = ele.jobs.find(ele => ele.name === 'driving (second shift)');
+      const secondShiftOTG = ele.jobs.find(ele => ele.name === 'otg (second shift)');
+      const breakdownCar = ele.jobs.find(ele => ele.name === 'breakdown car');
+      const breakdownVan = ele.jobs.find(ele => ele.name === 'breakdown van driver');
+      
+      const legacySetupCount = legacySetup?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+      const legacyFirstShiftDriverCount = legacyFirstShiftDrivers?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+      const legacyFirstShiftOTGCount = legacyFirstShiftOTG?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+      const legacySecondShiftDriverCount = legacySecondShiftDrivers?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+      const legacySecondShiftOTGCount = legacySecondShiftOTG?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+      const legacyBreakdownCount = legacyBreakdown?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+      const setupCarCount = setUpCar?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+      const setupVanCount = setupVan?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+      const firstShiftDriverCount = firstShiftDrivers?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+      const firstShiftOTGCount = firstShiftOTG?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+      const secondShiftDriverCount = secondShiftDrivers?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+      const secondShiftOTGCount = secondShiftOTG?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+      const breakdownCarCount = breakdownCar?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
+      const breakdownVanCount = breakdownVan?.jobassignments.reduce((acc, curr) => acc + curr.quantity, 0);
 
-      let halfDay = (firstShiftDriving + firstShiftOTG >= 2 || drivingFirstShift + otgFirstShift >= 2) ||
-                    (secondShiftDriving + secondShiftOTG >= 2 || drivingSecondShift + otgSecondShift >= 2);
-      let fullDay = (firstShiftDriving + firstShiftOTG >= 2 || drivingFirstShift + otgFirstShift >= 2) &&
-                    (secondShiftDriving + secondShiftOTG >= 2 || drivingSecondShift + otgSecondShift >= 2);    
-
+      let halfDay = (legacyFirstShiftDriverCount + legacyFirstShiftOTGCount >= 2 || firstShiftDriverCount + firstShiftOTGCount >= 2) ||
+                    (legacySecondShiftDriverCount + legacySecondShiftOTGCount >= 2 || secondShiftDriverCount + secondShiftOTGCount >= 2);
+      let fullDay = (legacyFirstShiftDriverCount + legacyFirstShiftOTGCount >= 2 || firstShiftDriverCount + firstShiftOTGCount >= 2) &&
+                    (legacySecondShiftDriverCount + legacySecondShiftOTGCount >= 2 || secondShiftDriverCount + secondShiftOTGCount >= 2); 
+                    
+      container.legacySetupCount = legacySetupCount ? legacySetupCount : null;
+      container.legacyFirstShiftDriverCount = legacyFirstShiftDriverCount ? legacyFirstShiftDriverCount : null;
+      container.legacyFirstShiftOTGCount = legacyFirstShiftOTGCount ? legacyFirstShiftOTGCount : null;
+      container.legacySecondShiftDriverCount = legacySecondShiftDriverCount ? legacySecondShiftDriverCount : null;
+      container.legacySecondShiftOTGCount = legacySecondShiftOTGCount ? legacySecondShiftOTGCount : null;
+      container.legacyBreakdownCount = legacyBreakdownCount ? legacyBreakdownCount : null;
       container.date = ele.edate ? ele.edate : null;
       container.month = ele.edate ? ele.edate.substring(5,7) : null;
       container.day = ele.edate ? ele.edate.substring(8,10) : null;
-      container.setup = setup ? setup : null;
-      container.setupCar = setupCar ? setupCar : null;
-      container.setupVan = setupVan ? setupVan : null;
-      container.firstShiftDriver = firstShiftDriving ? firstShiftDriving : null;
-      container.firstShiftOTG = firstShiftOTG ? firstShiftOTG : null;
-      container.drivingFirstShift = drivingFirstShift ? drivingFirstShift : null;
-      container.otgFirstShift = otgFirstShift ? otgFirstShift : null;
-      container.secondShiftDriver = secondShiftDriving ? secondShiftDriving : null;
-      container.secondShiftOTG = secondShiftOTG ? secondShiftOTG : null;
-      container.drivingSecondShift = drivingSecondShift ? drivingSecondShift : null;
-      container.otgSecondShift = otgSecondShift ? otgSecondShift : null;
-      container.breakdown = breakdown ? breakdown : null;
-      container.breakdownCar = breakdownCar ? breakdownCar : null;
-      container.breakdownVan = breakdownVan ? breakdownVan : null;
+      container.setupCarCount = setupCarCount ? setupCarCount : null;
+      container.setupVanCount = setupVanCount ? setupVanCount : null;
+      container.firstShiftDriverCount = firstShiftDriverCount ? firstShiftDriverCount : null;
+      container.firstShiftOTGCount = firstShiftOTGCount ? firstShiftOTGCount : null;
+      container.secondShiftDriverCount = secondShiftDriverCount ? secondShiftDriverCount : null;
+      container.secondShiftOTGCount = secondShiftOTGCount ? secondShiftOTGCount : null;
+      container.breakdownCarCount = breakdownCarCount ? breakdownCarCount : null;
+      container.breakdownVanCount = breakdownVanCount ? breakdownVanCount : null;
       container.halfDay = halfDay ? halfDay : null;
       container.fullDay = fullDay ? fullDay : null;
 
